1491aa8356e1c15c4fa768aa7d637ca08a5ab6f0
Roshni Bokade <you@example.comroshnibokade17@gmail.comgit config --global user.email you@example.comroshnibokade17@gmail.com>
2017-10-27 00:19:31 +0530

    initial setup

---
+++ SampleCoreApp/Controllers/AlbumController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SampleCoreApp.Models;
+
+namespace SampleCoreApp.Controllers
+{
+    public class AlbumController : Controller
+    {
+        public IActionResult Index()
+        {
+            MusicStoreContext context = HttpContext.RequestServices.GetService(typeof(SampleCoreApp.Models.MusicStoreContext)) as MusicStoreContext;
+
+            return View(context.GetAllAlbums());
+        }
+    }
+}
